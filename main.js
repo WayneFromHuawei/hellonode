5ef7fdf7e7e915a497081b685cb514b29c7c13c4
WayneFromHuawei <wayne.wan@huawei.com>
2018-04-04 12:57:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var http = require('http');
 // configure our HTTP server
 var server = http.createServer(function (request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
-  response.end("Hello, flexible engine, this is build 44\n");
+  response.end("Hello, flexible engine, this is build #46\n");
 });
 
 // listen on localhost:8000
-server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+server.listen(30000);
+console.log("Server listening at http://127.0.0.1:30000/");
